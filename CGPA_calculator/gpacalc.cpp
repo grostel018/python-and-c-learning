cfca6f3c59eb348d823915d52c8a6546a901efe5
ROSTEL EBELE GENI NDOUDI <142733884+grostel018@users.noreply.github.com>
2026-02-17 01:08:27 +0300

    Add plan and function outlines for GPA calculator

---
+++ CGPA_calculator/gpacalc.cpp
@@ -0,0 +1,13 @@
+//plan 
+//make a welcome function
+//for gpa : 
+//make an input function that only takes numbers and displays an error otherwise
+//make an input function that only takes strings and displays an error otherwise
+// 
+//make a lecture adder function that takes the lecture name and the total grade scored on that lecture
+//plus the weight of that lecture
+// 
+//implement my semester lecture average grade calculator to help doing this task (total grade scored on that lecture)
+//make a lecturescount that count the number of lectures taken in the semester
+// 
+// calculate cgpa based on the total semesters added : semester 1 gpa + semester 2 gpa + ... +n / n
