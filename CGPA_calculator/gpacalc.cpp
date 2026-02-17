c3beb4cde89d56b4db8e2c9d651ee1462acba03e
ROSTEL EBELE GENI NDOUDI <142733884+grostel018@users.noreply.github.com>
2026-02-18 00:31:12 +0300

    Implement input functions for GPA calculator

--- CGPA_calculator/gpacalc.cpp
+++ CGPA_calculator/gpacalc.cpp
@@ -11,3 +11,100 @@
 //make a lecturescount that count the number of lectures taken in the semester
 // 
 // calculate cgpa based on the total semesters added : semester 1 gpa + semester 2 gpa + ... +n / n
+
+#include <cctype>
+#include<iostream>
+#include <string>
+
+
+
+
+void welcome()
+{
+    std::cout << "\t*Welcome to the GPA calculator*" << std::endl;
+    std::cout << "\t*You can either compute your GPA or you CGPA*" << std::endl;
+}
+
+double takeNumber()
+{
+    std::cout << "Enter a number" << std::endl;
+    double number{ 0.0 };
+    while (true)
+    {
+        if (std::cin >> number)
+        {
+            std::cin.ignore(10000, '\n');
+            return number;  // No need for static_cast, number is already double
+        }
+        else
+        {
+            std::cout << "Enter a valid input (must be a number)" << std::endl;
+            std::cin.clear();           // Clear the error flags
+            std::cin.ignore(10000, '\n'); // Discard invalid input
+        }
+
+    }
+
+}
+
+
+std::string takeString()
+{   
+    std::string text  {""};
+    
+    std::cout << "Enter the lecture name (letters and spaces only):" << std::endl;
+
+    while (true)
+
+    {
+
+        std::getline(std::cin, text);
+    
+        bool valid = !text.empty();
+
+        for (char c : text) {
+            if (!std::isalpha(c) && c != ' ') {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid) {
+            return text;
+        }
+        else {
+            std::cout << "Invalid input! Use only letters and spaces.\n";
+        }
+
+
+    }
+
+    return text;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    welcome();
+    takeNumber();
+    takeString();
+
+    return 0;
+}
