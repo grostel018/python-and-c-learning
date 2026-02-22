a9a797ac06efc9fd887e571d73db271efff00b13
ROSTEL EBELE GENI NDOUDI <142733884+grostel018@users.noreply.github.com>
2026-02-22 23:07:57 +0300

    added sql and created an empty db

--- Grades manager/main.cpp
+++ Grades manager/main.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
 #include "UI.h"
 #include "Menu.h"
+#include "sqlite3.h"
 
 int main() {
+
+    sqlite3* db;
+    int exit = sqlite3_open("gpa.db", &db);
+
+    if (exit)
+        std::cout << "Error opening DB\n";
+    else
+        std::cout << "Database opened successfully!\n";
+
+    sqlite3_close(db);
+
     welcome();
     printCommands();
     menu();
